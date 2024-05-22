f245324dfea784a7d6167a8ed5fbf5fba1a4e3fd
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-05-22 15:53:21 -0600

    small modifications

--- techhistoria/src/Components/Search.js
+++ techhistoria/src/Components/Search.js
@@ -4,7 +4,7 @@ function Search({ setSearchTimeLine }) {
         <input 
         placeholder="Search 🔎" 
         onChange={(e) => setSearchTimeLine(e.target.value)} 
-        className="search sm:m-12 sm:text-sm lg:m-14 lg:text-xl xl:mx-36 xl:mb-20 xl:mb-40 xl:text-2xl 2xl:mb-30"/>
+        className="search sm:m-12 sm:text-sm lg:m-14 lg:text-xl xl:mx-36 xl:mb-20 xl:text-2xl 2xl:mb-30"/>
     )
 }
 
