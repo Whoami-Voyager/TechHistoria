63f530294c37d328da843c2b8e5c96dfbb84b733
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-07-12 21:27:38 -0600

    moved files where they should be

--- src/Components/TimeCard.js
+++ src/Components/TimeCard.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function TimeCard({ time, handleDetailLinkClick }) {
+function TimeCard({ time }) {
     // Renders all the images, years, titles, and a Link to the details of each post
     return (
         <div className="flex flex-col font-Inter border-b-4 rounded-8 p-8 xl:m-20 xl:flex-row">
@@ -8,7 +8,7 @@ function TimeCard({ time, handleDetailLinkClick }) {
             <div className="flex flex-col justify-center xl:ml-12">
                 <h1 className="text-4xl m-4 text-center xl:text-7xl xl:pt-4 xl:mb-4 2xl:m-20">{time.year}</h1>
                 <h3 className="text-2xl m-4 text-center xl:text-4xl xl:p-5 xl:mb-4 2xl:m-20">{time.title}</h3>
-                <Link to={`/history/${time.id}`} onClick={handleDetailLinkClick} className="button w-20">Details</Link>
+                <Link to={`/history/${time.id}`} className="button w-20">Details</Link>
             </div>
         </div>
     );
